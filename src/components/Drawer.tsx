9b3481d856db224fa6eaa4b75fc58b31d6569d62
Sin Wai Kit <31162981+wkitsin@users.noreply.github.com>
2022-01-16 16:43:32 +0800

    fix indentation in drawer file

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -4,41 +4,41 @@ import Icon from './Icon';
 const Drawer: React.FC = () => {
 	return (
 		<div className="shadow drawer bg-[#282830]">
-            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+			<input id="my-drawer" type="checkbox" className="drawer-toggle" />
 
-            <div className="flex flex-col w-screen">
-                <div className="w-full p-4 border-b border-slate-700">
-                    <label htmlFor="my-drawer" className="btn-primary drawer-button">
-                        <Icon
-                            className="cursor-pointer"
-                            name='drawer'
-                            colorFn={({ hover }: { hover: Boolean }) => hover ? 'white' : 'grey'}
-                        />
-                    </label>
-                </div>
-            </div>
+			<div className="flex flex-col w-screen">
+				<div className="w-full p-4 border-b border-slate-700">
+					<label htmlFor="my-drawer" className="btn-primary drawer-button">
+						<Icon
+							className="cursor-pointer"
+							name="drawer"
+							colorFn={({ hover }: { hover: Boolean }) => (hover ? 'white' : 'grey')}
+						/>
+					</label>
+				</div>
+			</div>
 
-            <div className="drawer-side">
-                <label htmlFor="my-drawer" className="drawer-overlay cursor-default h-screen z-10 absolute w-screen" />
-                <ul className="menu p-4 overflow-y-auto w-3/6 bg-base-100 text-base-content z-20 h-screen absolute">
-                    <li>
-                        <a>Wallet 1</a>
-                    </li>
-                    <li>
-                        <a>Wallet 2</a>
-                    </li>
-                    <li>
-                        <a>Wallet 3</a>
-                    </li>
-                    <li>
-                        <a>Wallet 4</a>
-                    </li>
-                    <li>
-                        <a>Wallet 5</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+			<div className="drawer-side">
+				<label htmlFor="my-drawer" className="drawer-overlay cursor-default h-screen z-10 absolute w-screen" />
+				<ul className="menu p-4 overflow-y-auto w-3/6 bg-base-100 text-base-content z-20 h-screen absolute">
+					<li>
+						<a>Wallet 1</a>
+					</li>
+					<li>
+						<a>Wallet 2</a>
+					</li>
+					<li>
+						<a>Wallet 3</a>
+					</li>
+					<li>
+						<a>Wallet 4</a>
+					</li>
+					<li>
+						<a>Wallet 5</a>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
 };
 
